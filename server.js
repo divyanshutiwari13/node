ac4497cf05ec789d3f11c9e2ea7c3a04a6032730
DIVYANSHU TIWARI <divyanshu1300@gmail.com>
2025-05-19 19:08:32 +0530

    server updated

--- server.js
+++ server.js
@@ -1,38 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/crud_demo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.get('/', (req, res) => {
+    res.send('Server is running on port 3000');
 });
 
-const Item = mongoose.model('Item', new mongoose.Schema({
-    name: String,
-    value: Number
-}));
-
-console.log("hello");
-app.get('/items', async (req, res) => {
-    const items = await Item.find();
-    res.json(items);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
-
-app.post('/items', async (req, res) => {
-    const item = new Item(req.body);
-    await item.save();
-    res.status(201).json(item);
-});
-
-app.put('/items/:id', async (req, res) => {
-    const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(item);
-});
-
-app.delete('/items/:id', async (req, res) => {
-    await Item.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Deleted' });
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
